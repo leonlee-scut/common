28b93006df842e28b5769296aa32d9a984b6e4ff
Leon@Leon-3240C <leonlee.scut@gmail.com>
2026-06-12 16:35:20 +0800

    添加公共代码层和头文件

---
+++ inc/common.h
@@ -0,0 +1,29 @@
+/**
+  * @file common.h
+  * @brief Common header file for the project.
+  * @author Leon Lee (leonlee.scut@outlook.com)
+  * @version 0.1
+  * @date 2026/06/12
+  * 
+  * @copyright Copyright (c) 2026 South China University of Technology.
+  * All rights reserved.
+  * 
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * 
+  * @par History:
+  * 	Date		Version		Author			Description
+  * 	2026/06/12	0.1			Leon Lee		Preliminary version.
+  */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include "assert.h"
+#include "critical_section.h"
+#include "fifo.h"
+#include "fxn.h"
+
+#endif /* __COMMON_H__ */
+/************* (C) COPYRIGHT South China Univ. of Tech. ****** END OF FILE ****/
